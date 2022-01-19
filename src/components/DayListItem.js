82bbfdcf1752aa1bed62e32512913589dbb13caa
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-01-18 16:50:38 -0800

    day list item component js styling and test file made

---
+++ src/components/DayListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classNames(
+    'day-list__item',{
+    'day-list__item--selected':props.selected,
+    'day-list__item--full':!props.spots
+  });
+
+  const formatSpots = (spots) => {
+    let text = spots? spots : "no";
+    text += spots === 1? " spot remaining": " spots remaining";
+    return text;
+  }
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)} >
+      <h2 className="text--regular">{props.name}</h2>
+      <h2 className="text--light">{formatSpots(props.spots)}</h2>
+    </li>
+  )
+}
